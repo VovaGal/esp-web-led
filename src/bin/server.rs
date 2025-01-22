25ffea3479f7b1208ead53b71e5b6179ffc453d7
VovaGal <vovagal.7@mail.ru>
2025-01-22 15:31:38 +0300

    two channel rmt for leds and 1 ledc pwm for buzzer

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -11,8 +11,13 @@ use embedded_svc::{
     wifi::{self, AccessPointConfiguration, AuthMethod},
 };
 
+use embedded_hal_0_2::PwmPin;
+use esp_idf_hal::{
+    ledc::{LowSpeed, SpeedMode},
+    prelude::*,
+};
 use esp_idf_svc::hal::{
-    ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver},
+    ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver, Resolution},
     prelude::Peripherals,
     units::Hertz,
 };
@@ -29,6 +34,9 @@ use serde::Deserialize;
 
 use rgb_led::{RGB8, WS2812RMT};
 
+use esp_idf_hal::sys::esp;
+use esp_idf_hal::sys::ledc_set_freq;
+
 const SSID: &str = env!("WIFI_SSID");
 const PASSWORD: &str = env!("WIFI_PASS");
 static INDEX_HTML: &str = include_str!("../http_server_page.html");
@@ -44,10 +52,10 @@ const CHANNEL: u8 = 11;
 
 #[derive(Deserialize, Debug)]
 struct FormData {
-    r: f32,
-    g: f32,
-    b: f32,
-
+    r: u32,
+    g: u32,
+    b: u32,
+    f: u32,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -56,8 +64,20 @@ fn main() -> anyhow::Result<()> {
 
     let peripherals = Peripherals::take()?;
 
-    let mut led = WS2812RMT::new(peripherals.pins.gpio10, peripherals.rmt.channel0)?;
-    led.set_pixel(RGB8::new(50, 50, 0))?;
+    let mut ws_led = WS2812RMT::new(peripherals.pins.gpio5, peripherals.rmt.channel0)?;
+    ws_led.set_pixel(RGB8::new(0, 10, 0))?;
+
+    let mut chip_led = WS2812RMT::new(peripherals.pins.gpio10, peripherals.rmt.channel1)?;
+    chip_led.set_pixel(RGB8::new(10, 0, 0))?;
+
+    // Configure and Initialize LEDC Timer Driver
+    let mut timer_driver = LedcTimerDriver::new(
+        peripherals.ledc.timer0,
+        &TimerConfig::default()
+            .frequency(1000.Hz())
+            .resolution(Resolution::Bits14),
+    )
+    .unwrap();
 
     // Setup Wifi
 
@@ -66,15 +86,12 @@ fn main() -> anyhow::Result<()> {
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    led.set_pixel(RGB8::new(0, 0, 50))?;
-
-
     let mut wifi = BlockingWifi::wrap(
         EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
         sys_loop,
     )?;
 
-//
+    //
 
     connect_wifi(&mut wifi)?;
 
@@ -107,11 +124,9 @@ fn main() -> anyhow::Result<()> {
             // write!(resp, "Red: {} Green: {} Blue: {}", form.r, form.g, form.b)?;
             write!(resp, "Form: {:#?}", form)?;
 
-
             {
                 // *dur.lock().unwrap() = form.age;
                 sendr.send(form).unwrap();
-                
             }
         } else {
             resp.write_all("JSON error".as_bytes())?;
@@ -122,8 +137,6 @@ fn main() -> anyhow::Result<()> {
 
         Ok(())
     })?;
-
-
 
     // Keep wifi and the server running beyond when main() returns (forever)
     // Do not call this if you ever want to stop or access them later.
@@ -137,9 +150,24 @@ fn main() -> anyhow::Result<()> {
     // Main task no longer needed, free up some memory
 
     let thread = thread::spawn(move || {
+        let mut driver = LedcDriver::new(
+            peripherals.ledc.channel0,
+            timer_driver,
+            peripherals.pins.gpio4,
+        )
+        .unwrap();
+        //
         loop {
             let colour = recvr.recv().unwrap();
-            led.set_pixel(RGB8::new(colour.g as u8, colour.r as u8, colour.b as u8));
+            esp!(unsafe { ledc_set_freq(LowSpeed::SPEED_MODE, driver.timer(), colour.f) }).unwrap();
+            // timer_driver.set_frequency(colour.f.Hz()).unwrap();
+            ws_led
+                .set_pixel(RGB8::new(colour.r as u8, colour.g as u8, colour.b as u8))
+                .unwrap();
+            chip_led
+                .set_pixel(RGB8::new(colour.g as u8, colour.r as u8, colour.b as u8))
+                .unwrap();
+            driver.set_duty(driver.get_max_duty() / 2).unwrap();
         }
     });
 
